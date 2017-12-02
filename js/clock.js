8239445db0938537fdc5930f30875c11189841f1
iftikharkhana44 <iftikharkhana44@gmail.com>
2017-12-02 16:15:53 +0000

    combined both clock javascripts together into one file and styles the clocks inside css

--- js/clock.js
+++ js/clock.js
@@ -3,4 +3,11 @@ let date = new Date();
 let hour = date.getHours();
 let minutes = date.getMinutes();
 
-clocksw.innerHTML = hour + " : " + minutes;
+clocksw.innerHTML = hour + 5 + " : " + minutes;
+
+let clockuk = document.getElementById("clockuk");
+let date1 = new Date();
+let hour1 = date1.getHours();
+let minutes1 = date1.getMinutes();
+
+clockuk.innerHTML = hour1 + " : " + minutes1;
