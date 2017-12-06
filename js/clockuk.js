c3854e2cff0b587ea47be18ac598aac6b4a70457
iftikharkhana44 <iftikharkhana44@gmail.com>
2017-12-06 11:31:18 +0000

    added meta for mobile

--- js/clockuk.js
+++
@@ -1,6 +0,0 @@
-let clockuk = document.getElementById("clockuk");
-let date1 = new Date();
-let hour1 = date1.getHours();
-let minutes1 = date1.getMinutes();
-
-clockuk.innerHTML = hour1 + " : " + minutes1;
